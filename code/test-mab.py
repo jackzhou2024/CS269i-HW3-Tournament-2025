c84afcfec6d8e7c90512ff1dcc26c1803c241635
jackzhou2024 <166473088+jackzhou2024@users.noreply.github.com>
2025-04-24 23:10:53 -0700

    Add files via upload

---
+++ code/test-mab.py
@@ -0,0 +1,20 @@
+# An example that shows how to use the UCB1 learning policy
+# to make decisions between two arms based on their expected rewards.
+
+# Import MABWiser Library
+from mabwiser.mab import MAB, LearningPolicy, NeighborhoodPolicy
+
+# Data
+arms = ['Arm1', 'Arm2']
+decisions = ['Arm1', 'Arm1', 'Arm2', 'Arm1']
+rewards = [20, 17, 25, 9]
+
+# Model
+mab = MAB(arms, LearningPolicy.UCB1(alpha=1.25))
+
+# Train
+mab.fit(decisions, rewards)
+
+# Test
+p = mab.predict()
+print(p)
